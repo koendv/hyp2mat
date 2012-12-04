c6a4e32cb43d8144ef659f9785f7fffa6dbc8873
koen <kdv@kdvelectronics>
2012-12-04 17:29:00 +0100

    tutorial

--- eagle/tutorial/tutorial_hyp2mat.m
+++ eagle/tutorial/tutorial_hyp2mat.m
@@ -96,6 +96,8 @@ Sim_CSX = 'msl.xml';
 [status, message, messageid] = rmdir(Sim_Path, 's'); % clear previous directory
 [status, message, messageid] = mkdir(Sim_Path ); % create empty simulation folder
 
+disp([ 'Estimated simulation runtime: 55000 timesteps' ]); % inform user this may take a while... 
+
 WriteOpenEMS([Sim_Path '/' Sim_CSX], FDTD, CSX);
 CSXGeomPlot([Sim_Path '/' Sim_CSX]);
 RunOpenEMS(Sim_Path, Sim_CSX);
