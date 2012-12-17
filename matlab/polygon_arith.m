ea1afaafb05af4ee063f2214b90cf8e5b540b7e1
koen <kdv@kdvelectronics>
2012-12-17 10:46:38 +0100

    polygon arithmetic

---
+++ matlab/polygon_arith.m
@@ -0,0 +1,58 @@
+%pkg load geometry
+% p1 = [ 0 0;  1 0  ; 1 1; 0 1 ; 0 0 ];
+% p2 = 2 * p1;
+% polygon_arith(p1, '+', p2);
+
+function q = polygon_arith(p1, operand, p2);
+
+  % trivial cases 
+  if isempty(p1)
+    if strcmp(operand, '+')
+      q = p2;
+    else
+      q = [];
+    end;
+    return;
+  end;
+
+  if isempty(p2)
+    q = p1;
+    return;
+  end;
+
+  % non-trivial cases: use polybool in matlab, oc_polybool in octave
+  if exist('OCTAVE_VERSION', 'builtin')
+
+    % octave
+    if strcmp(operand, '+')
+      action = 'AND';
+    elseif strcmp(operand, '-') 
+      action = 'AB';
+    else
+      error ([ 'unknown operand ' operand]);
+    end
+
+    [qx, qy, npol, nint, npert] = oc_polybool(p1, p2, action);
+    q = [qx, qy];
+  
+  else
+  
+    % matlab
+    p1x = p1(:,1).';
+    p1y = p1(:,2).';
+  
+    p2x = p2(:,1).';
+    p2y = p2(:,2).';
+  
+    if (~strcmp(operand, '+') && ~strcmp(operand, '-'))
+      error ([ 'unknown operand ' operand]);
+    end
+  
+    [qx, qy] = polybool (action, p1x, p1y, p2x, p2y);
+  
+    q = [qx.', qy.'];
+  end
+ 
+end 
+% not truncated
+
---
+++ matlab/polygon_extend.m
@@ -0,0 +1,40 @@
+% p = [ 0 0;  1 0  ; 1 1; 0 1 ; 0 0 ];
+% polygon_extend(p, 0.5);
+
+function q = polygon_extend(p, width);
+
+  % trivial case
+  if (width == 0)
+    q = p;
+    return;
+  end
+
+  if (isempty(p))
+    q = p;
+    return;
+  end
+
+  % use bufferm2 in matlab, expandPolygon in octave
+  if exist('OCTAVE_VERSION', 'builtin')
+    % octave
+    if ~exist('expandPolygon')
+      pkg load geometry;
+    end
+
+    % Alternative: q = polyjoin(extendPoly(p, width, pi/4)); % google "Accurate Polygon Extension"
+    q = expandPolygon(p, width); % XXX not 100% correct: behavior on corners is different.
+
+  else
+    % matlab
+    px = p(:,1).';
+    py = p(:,2).';
+
+    % [qx,  qy] = bufferm2('xy', px, py, width, 'out', 4, 'vector');
+    [qx,  qy] = bufferm2('xy', px, py, width, 'out', 16, 'vector');
+    q = [qx, qy];
+    
+  end
+ 
+end 
+% not truncated
+
