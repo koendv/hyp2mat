e6ec196a6697d9cad4529d5437ee5b1e6eeebe58
koen <kdv@kdvelectronics>
2013-07-19 14:18:58 +0200

    AddHyperLynxComponent

--- matlab/AddHyperLynxComponent.m
+++ matlab/AddHyperLynxComponent.m
@@ -1,42 +1,73 @@
-function CSX = AddHyperLynxComponent(CSX, name, prio, port1, port2, direction, varargin)
-% function CSX = AddHyperLynxComponent(CSX, name, prio, port1, port2, direction, varargin)
+function CSX = AddHyperLynxComponent(CSX, name, prio, port1, port2, varargin)
+% function CSX = AddHyperLynxComponent(CSX, name, prio, port1, port2, varargin)
 %
-% Add a two-port lumped element to the CSX model.
+% Add a resistor, capacitor or inductance to a CSX model imported from HyperLynx.
+% Component layout is limited to x or y direction, and top or bottom layers of pcb.
 % 
 % arguments:
-%   name:       component name, e.g. 'R1'
-%   prio:       component priority
+%   name:       component name.
+%   prio:       component priority.
 %   port1:      component pad name. 
 %   port2:      component pad name. 
-%   direction:  0 for x-orienation, 1 for y-orientation of the lumped element
 % 
 % optional arguments:
 %   'R', 'C', 'L':  definition of the lumped element properties
 %
 % examples:
-% lumped element capacitor in x-direction with 1pF
-% CSX = AddHyperLynxComponent( CSX, 'C1', 0, 'C1.1', 'C1.2', 'C', 1e-12 );
+% 50R resistor between pads R1.1 and R1.2 with 1pF parasitic capacitance:
+% CSX = AddHyperLynxComponent( CSX, 'R1', 'R1.1', 'R1.2', 'R', 50, 'C', 1e-12 );
 %
-% See also AddLumpedElement, AddBox
+% See also GetHyperLynxPort, AddLumpedElement
 %
+
+% Copyright 2012 Koen De Vleeschauwer.
+%
+% This file is part of hyp2mat.
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+% Check CSX contains HyperLynx port list.
+
+  if (~isfield(CSX, 'HyperLynxPort'))
+    error('CSX not created by ImportHyperLynx()');
+  end
 
 % Get pad coordinates pad_start and pad_stop. pad position is either 'top' or 'bottom'. 
 
 [position1, pad1_start, pad1_stop] = GetHyperLynxPort(CSX, port1);
 [position2, pad2_start, pad2_stop] = GetHyperLynxPort(CSX, port2);
 
-% Layout component
-if (direction == 0)
+% Determine component orientation
+pad1c=(pad1_start+pad1_stop)/2;
+pad2c=(pad2_start+pad2_stop)/2;
+v=pad2c-pad1c;
+theta=atan2(v(2), v(1)); % theta between -pi and pi.
+vertical=(abs(theta) > pi/4) && (abs(theta) < 3*pi/4); % true if component has vertical orientation
+
+% layout component
+if (vertical)
+  % layout component in y-direction
+  comp_height = abs(pad1_stop(2) - pad1_start(2));
+  comp_start  = [pad1_start(1), (pad1_start(2)+pad1_stop(2))/2, pad1_start(3)];
+  comp_stop   = [pad2_stop(1), (pad2_start(2)+pad2_stop(2))/2, pad2_stop(3)];
+  direction = 1;
+else
   % layout component in x-direction
   comp_height = abs(pad1_stop(1) - pad1_start(1));
   comp_start  = [(pad1_start(1)+pad1_stop(1))/2, pad1_start(2), pad1_start(3)];
   comp_stop   = [(pad2_start(1)+pad2_stop(1))/2, pad2_stop(2), pad2_stop(3)];
-elseif (direction == 1)
-  comp_height = abs(pad1_stop(2) - pad1_start(2));
-  comp_start  = [pad1_start(1), (pad1_start(2)+pad1_stop(2))/2, pad1_start(3)];
-  comp_stop   = [pad2_stop(1), (pad2_start(2)+pad2_stop(2))/2, pad2_stop(3)];
-else
-  error('component direction not 0 or 1');
+  direction = 0;
 end
 
 % Component position has to be 'top' or 'bottom'. 
--- matlab/Contents.m
+++ matlab/Contents.m
@@ -2,6 +2,6 @@
 % 
 % ImportHyperLynx.m         Import Hyperlynx file into CSX model.
 % GetHyperLynxPort.m        Obtain coordinates and material of HyperLynx pin or pad.
-% AddHyperLynxComponent.m   Add a two-port lumped element to the CSX model.
+% AddHyperLynxComponent.m   Add a resistor, capacitor or inductance to a CSX model imported from HyperLynx.
 %
 
