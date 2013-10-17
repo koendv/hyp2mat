f46dc3afa31e44f20f8ad0e6680ce984f15e8ac6
Koen De Vleeschauwer <kdv@kdvelectronics.eu>
2013-10-17 14:32:17 +0200

    don't call aclocal; use autoreconf

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-
-mkdir config 2> /dev/null
-
-libtoolize -i || exit 1
-aclocal || exit 1
-automake --gnu --add-missing || exit 1
-autoheader configure.ac || exit 1
-autoconf
+autoreconf -i
